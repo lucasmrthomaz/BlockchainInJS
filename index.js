bb202854a0cc3aa58023c62d384caa5fc432c59d
Lucas Thomaz <lucasmrthomaz@gmail.com>
2020-05-08 03:30:39 -0300

    Adicao dos arquivos de api e proof o work

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 var express = require('express');
 var app = express();
+const API = require('./API')
 const Blockchain = require('./blockchain')
+const proofWorkStd = require('./proofWork')
+
 
 //Instancia/Inicia um novo Blockchain
 const blockchain = new Blockchain()
 
+//Inicia um proof
+//const proofWorkstd = new proofWorkStd()
+
 //Adiciona quantos blocos quiser
-blockchain.addBlock({ amount: 4 })
-blockchain.addBlock({ amount: 50 })
-blockchain.addBlock({ amount: 100 })
+//blockchain.addBlock({ amount: 4 })
+//blockchain.addBlock({ amount: 50 })
+//blockchain.addBlock({ amount: 100 })
 blockchain.addBlock({ amount: 150 })
 
 //Imprime no console os blocos adicionados ao chain
 console.log(blockchain)
+
+//Faz o trabalho de proof
+//console.log(proofWork.doWork())
 
 //Verifica se eh um bloco valido
 console.log(blockchain.isValid())
 blockchain.blocks[1].data.amount = 30000
 console.log(blockchain.isValid())
 
-/* express: Ao digitar no browser
-localhost:3000 retornar um objeto JSON */
-app.get('/', function (req, res) {
-    const JSON_Object = JSON.stringify(blockchain)
-    res.send(blockchain)
-});
 
-/* express: Efetiva a inicilizacao de 
-uma nova instancia do express */
-app.listen(3000, function () {
-  console.log('Express rodando na porta 3000!');
-});
---
+++ proofWork.js
@@ -0,0 +1,7 @@
+const Blockchain = require('./blockchain')
+const Block = require('./block')
+const crypto = require('crypto')
+        
+
+
+module.exports = "proofWork"
